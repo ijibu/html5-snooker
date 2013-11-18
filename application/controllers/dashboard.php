40b17ad891b02931fd2efedd152c4fe4fd3924b1
work <work@ijibu.com>
2013-11-18 16:52:59 +0800

    修改获取当前在线玩家接口 Signed-off-by: work <work@ijibu.com>

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -8,11 +8,12 @@ class Dashboard extends MY_Controller {
 	public function __construct() 
 	{
 		parent::__construct();
+		$this->load->model('user_model');
 	}
 	
 	public function index() 
 	{
-		if ($_GET['info'] == 'true') {
+		if (isset($_GET['info']) == 'true') {
 			$cacheFile = 'data/join.php';
 			$data = array();
 			if (file_exists($cacheFile)) {
@@ -36,21 +37,39 @@ class Dashboard extends MY_Controller {
 						),
 					),
 				);
-			} else {
+			} else {			//获取在线的玩家
 				$ret = array(
-					"nicks" => array("5204505f34f714.00821883" => array("nick" => "ijibu","playing" => true)),
+					"nicks" => array(
+						"5204505f34f714.00821883" => array("nick" => "ijibu","playing" => true),
+						"5204505f34f714.00821881" => array("nick" => "ijibu1","playing" => true)
+					),
 					"invited" => false
 				);
 			}
 		
 		} else {
-			if (isset($_GET['nick'])) {
+			if (isset($_GET['nick'])) {			//新用户上线，加入在在线用户中。
 				$_SESSION['user_name'] = $_GET['nick'];
+				$this->user_model->addUser(array('nick' => $_GET['nick']));
 			}
-		    $ret = array("status" => ok); 
+		    $ret = array("status" => 'ok'); 
 		}
 		
 		echo json_encode($ret);exit;
 
+	}
+
+	/**
+	 * 获取在线的用户
+	 */
+	private function getOnlinePlayers()
+	{
+		$data = $this->user_model->getUsers();
+		$onlinePlayers = array();
+		foreach ($data as $key => $value) {
+			if ($value['nick'] != $_SESSION['user_name']) {
+				$onlinePlayers[] = $value;
+			}
+		}
 	}
 }
